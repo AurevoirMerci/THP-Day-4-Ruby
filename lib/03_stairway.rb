d9f4327b0694899cd8d3b52432256a0bfaee63fe
AdrienPeligry <adrien.peligry@essec.edu>
2019-01-10 19:04:41 +0100

    final push

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,11 +1,13 @@
 def position
-
 marches = 0
+coups = 0 
 
 while marches != 10
 
-	puts "Lance le dé en appuyant sur Entrée"
-	gets
+coups = coups + 1 
+
+	#puts "Lance le dé en appuyant sur Entrée"
+	#gets
 
 	dés = 1 + rand(6)
 
@@ -31,7 +33,43 @@ end
 end
 end
 
-puts "T'es chaud on peut dire que t'as ramené la coupe à la maison maestro"
+puts "T'es chaud on peut dire que t'as ramené la coupe à la maison maestro et tu as fini en #{coups} coups"
+return coups 
 end
 
-position
+
+
+
+
+def average_finish_time
+	
+coupsdupatron = []
+i = 1
+
+while i < 100
+	coups = position
+	coupsdupatron << coups 
+
+i = i + 1
+
+end
+	resultat = coupsdupatron.inject {|sum, el| sum + el }.to_i / coupsdupatron.size
+
+puts "La moyenne des 100 jeux est de #{resultat}." 
+
+end
+
+def perform
+
+	average_finish_time
+end
+
+perform
+
+
+
+
+
+
+
+
